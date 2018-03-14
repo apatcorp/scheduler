339478fb79b891030852861f06df6c4380ca0f73
apatcorp <funplayer92@gmx.de>
2018-03-14 13:15:37 +0100

    Changes to the Date Picker

--- src/main/java/NewDailyRoutineController.java
+++ src/main/java/NewDailyRoutineController.java
@@ -1,50 +1,77 @@
-import custom.CustomDatePicker;
+import com.calendarfx.view.YearMonthView;
+import javafx.collections.SetChangeListener;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
+import javafx.scene.input.MouseEvent;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NewDailyRoutineController extends Controller {
 
     @FXML
-    public CustomDatePicker datePicker;
+    public YearMonthView datePicker;
 
-    private List<LocalDate> selectedDates = new ArrayList<>();
+    private static LocalDate startDate = null, endDate = null;
+
+    public static List<CustomDateCell> customDateCells = new ArrayList<>();
+    public static List<LocalDate> selectedDates = new ArrayList<>();
 
     @Override
     public void setup() {
         super.setup();
 
-        datePicker.setOnMouseEntered(event -> {
-            if (datePicker.isShowing()) {
-                datePicker.setDateSelected(false);
-                datePicker.hide();
-            }
-        });
+        datePicker.setCellFactory(param ->{
+            CustomDateCell dateCell = new CustomDateCell(param.getDate());
 
-        datePicker.setOnAction(event -> {
-            if (!selectedDates.contains(datePicker.getValue()))
-                selectedDates.add(datePicker.getValue());
-            else
-                selectedDates.remove(datePicker.getValue());
+            /*dateCell.setOnMouseClicked(event -> {
+                dateCell.clicked = !dateCell.clicked;
+                dateCell.setStyle(dateCell.clicked ? "-fx-background-color: cyan;" : dateCell.getStyle());
 
-            datePicker.setDateSelected(true);
+                if (startDate == null && endDate == null) {
+                    startDate = dateCell.getDate();
+                    reset();
+                } else if (startDate != null && endDate == null)
+                    endDate = dateCell.getDate();
 
-            System.out.println("Test");
-        });
+                if (startDate != null && endDate != null) {
+                    if (startDate.isAfter(endDate)) {
+                        LocalDate tmp = endDate;
+                        endDate = startDate;
+                        startDate = tmp;
+                    }
 
-        datePicker.setDayCellFactory(param ->
-            new DateCell() {
-                @Override
-                public void updateItem(LocalDate item, boolean empty) {
-                    super.updateItem(item, empty);
-                    setStyle(selectedDates.contains(item) ? "-fx-background-color: cyan;" : "");
-                    setDisabled(false);
-                    setEditable(true);
+                    selectedDates.addAll(Stream.iterate(startDate, date -> date.plusDays(1)).limit(ChronoUnit.DAYS.between(startDate, endDate) + 1).collect(Collectors.toList()));
+                    System.out.println(selectedDates);
 
+                    setRange();
+
+                    startDate = endDate = null;
                 }
-            });
+            });*/
+
+            return dateCell;
+        });
+
+
+        datePicker.getSelectedDates().addListener((SetChangeListener<? super LocalDate>) change -> {
+            if (change.wasAdded())
+                System.out.println(change.getElementAdded());
+        });
+    }
+
+    private void setRange () {
+        customDateCells.forEach(customDateCell -> {
+            customDateCell.setStyle(
+                    selectedDates.contains(customDateCell.getDate()) &&
+                    !customDateCell.getDate().equals(startDate) &&
+                    !customDateCell.getDate().equals(endDate)? "-fx-background-color: green;" : customDateCell.getStyle());
+        });
     }
 }
