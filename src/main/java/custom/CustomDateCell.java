c1f7eb8b4121826db92505c884de0c6a5dcb0930
apatcorp <funplayer92@gmx.de>
2018-03-18 19:39:12 +0100

    Fixed mulit selection in date picker

--- src/main/java/custom/CustomDateCell.java
+++ src/main/java/custom/CustomDateCell.java
@@ -2,6 +2,7 @@ package custom;
 
 import com.calendarfx.view.YearMonthView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseButton;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
@@ -13,22 +14,23 @@ public class CustomDateCell extends YearMonthView.DateCell {
 
     private YearMonthView datePicker;
     private List<LocalDate> highlightedDates;
+    private static List<LocalDate> minMaxDates;
 
-    private String standardStyle, selectedStyle, rangeSelectedStyle, highlightedStyle;
+    private String standardStyle, selectedStyle, highlightedStyle;
 
     public CustomDateCell(YearMonthView datePicker) {
         this.datePicker = datePicker;
         highlightedDates = new ArrayList<>();
+        minMaxDates = new ArrayList<>();
 
         standardStyle = getStyle();
         selectedStyle = "-fx-background-color: #60E8AD;";
-        rangeSelectedStyle = "-fx-background-color: #B0E0E6;";
         highlightedStyle = "-fx-background-color: #E9967A;";
 
         this.setOnMouseClicked(event -> {
-            if (datePicker.getSelectedDates().size() == 2) {
+            /*if (datePicker.getSelectedDates().size() == 2) {
                 setRange();
-            }
+            }*/
         });
     }
 
@@ -36,16 +38,23 @@ public class CustomDateCell extends YearMonthView.DateCell {
         this.datePicker = datePicker;
         this.highlightedDates = new ArrayList<>();
         this.highlightedDates.addAll(highlightedDates);
+        minMaxDates = new ArrayList<>();
 
         standardStyle = getStyle();
         selectedStyle = "-fx-background-color: #60E8AD;";
-        rangeSelectedStyle = "-fx-background-color: #B0E0E6;";
         highlightedStyle = "-fx-background-color: #E9967A;";
         datePicker.setSelectionMode(SelectionMode.MULTIPLE);
 
         this.setOnMouseClicked(event -> {
-            if (datePicker.getSelectedDates().size() == 2) {
-                setRange();
+            if (event.getButton().equals(MouseButton.SECONDARY)) {
+                if (!datePicker.getSelectedDates().contains(getDate()))
+                    datePicker.getSelectedDates().add(getDate());
+
+                minMaxDates.add(getDate());
+                if (minMaxDates.size() == 2) {
+                    setRange();
+                    minMaxDates.clear();
+                }
             }
         });
     }
@@ -57,9 +66,7 @@ public class CustomDateCell extends YearMonthView.DateCell {
         setDisable(date.isBefore(LocalDate.now()));
 
         if (datePicker.getSelectedDates().contains(date)) {
-            LocalDate first = datePicker.getSelectedDates().stream().reduce((localDate, localDate2) -> localDate).get();
-            LocalDate last = datePicker.getSelectedDates().stream().reduce((localDate, localDate2) -> localDate2).get();
-            setStyle(first.equals(date) || last.equals(date) ? selectedStyle: rangeSelectedStyle);
+            setStyle(selectedStyle);
         } else if (highlightedDates.contains(date)){
             setStyle(highlightedStyle);
         } else {
@@ -68,7 +75,16 @@ public class CustomDateCell extends YearMonthView.DateCell {
     }
 
     private void setRange () {
-        LocalDate startDate = datePicker.getSelectedDates().stream().reduce((localDate, localDate2) -> localDate).get();
+        LocalDate startDate = minMaxDates.get(0);
+        LocalDate endDate = minMaxDates.get(1);
+        if (startDate.isAfter(endDate)) {
+            LocalDate tmp = endDate;
+            endDate = startDate;
+            startDate = tmp;
+        }
+
+        datePicker.getSelectedDates().addAll(Stream.iterate(startDate, date -> date.plusDays(1)).limit(ChronoUnit.DAYS.between(startDate, endDate) + 1).collect(Collectors.toList()));
+        /*LocalDate startDate = datePicker.getSelectedDates().stream().reduce((localDate, localDate2) -> localDate).get();
         LocalDate endDate = datePicker.getSelectedDates().stream().reduce((localDate, localDate2) -> localDate2).get();
 
         if (startDate.isAfter(endDate)) {
@@ -79,5 +95,6 @@ public class CustomDateCell extends YearMonthView.DateCell {
 
         datePicker.getSelectedDates().clear();
         datePicker.getSelectedDates().addAll(Stream.iterate(startDate, date -> date.plusDays(1)).limit(ChronoUnit.DAYS.between(startDate, endDate) + 1).collect(Collectors.toList()));
+        */
     }
 }
